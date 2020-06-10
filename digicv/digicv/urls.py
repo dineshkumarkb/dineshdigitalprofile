7fa12c9e695fec64a4a7ced0299d0462e2e86afa
dineshkumarkb <dineshkumarkb@gmail.com>
2020-06-10 19:49:08 +0530

    Added views and app

--- digicv/digicv/urls.py
+++ digicv/digicv/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cv_app import views
 
 urlpatterns = [
+    path('',views.root_page),
     path('admin/', admin.site.urls),
 ]
