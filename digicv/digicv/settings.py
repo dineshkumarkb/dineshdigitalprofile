71581aab778bd61dca82a52d7484a44cc0ebf945
dineshkumarkb <dineshkumarkb@gmail.com>
2020-06-12 12:40:57 +0530

    Added static files

--- digicv/digicv/settings.py
+++ digicv/digicv/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 
 
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+STATIC_FILES_DIR = [
+    STATIC_DIR,
+]
