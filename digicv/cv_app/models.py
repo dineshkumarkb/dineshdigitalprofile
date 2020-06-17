b15683538ae103e9d4fa7a69ab6a733575cfc92f
dineshkumarkb <dineshkumarkb@gmail.com>
2020-06-17 11:40:41 +0530

    Latest changes

--- digicv/cv_app/models.py
+++ digicv/cv_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_name = models.CharField(max_length=10,unique=True)
+    user_email = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.user_name
