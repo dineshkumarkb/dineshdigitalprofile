a2eb867b60dd31c74827d1f7e7642e7c0d1c59d1
dineshkumarkb <dineshkumarkb@gmail.com>
2020-06-29 10:13:47 +0530

    Django changes

--- digicv/cv_app/urls.py
+++ digicv/cv_app/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-
 from cv_app import views
+
+app_name = 'cv_app'
 
 urlpatterns = [
     path('', views.root_page),
