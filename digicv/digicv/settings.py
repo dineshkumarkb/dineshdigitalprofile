731ef2d9d4040c724e894ed540d2304a4087fbec
dineshkumarkb <dineshkumarkb@gmail.com>
2020-07-13 21:10:32 +0530

    Latest background color changes

--- digicv/digicv/settings.py
+++ digicv/digicv/settings.py
@@ -89,6 +89,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
