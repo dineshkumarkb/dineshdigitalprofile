6839b7cfafb8758b47a6e010092a69a206fb43c6
dineshkumarkb <dineshkumarkb@gmail.com>
2020-07-14 20:37:31 +0530

    Latest refactored backend code

--- digicv/cv_app/views.py
+++ digicv/cv_app/views.py
@@ -9,12 +9,10 @@ import os
 
 # Create your views here.
 
-
-def root_page(request):
-    resume_contents = { 'resume_title': '''Python Cloud Developer. 11 years of industry experience in developing 
-    applications using Python, Android | 
-    AWS Certified solutions architect associate |
-    Certified scrum master''',
+CONTEXT_DICT = { 'resume_title': '''Python Cloud Developer. 11 years of industry experience in developing 
+                 applications using Python, Android | 
+                 AWS Certified solutions architect associate |
+                 Certified scrum master''',
                         'stack_url':'https://stackoverflow.com/users/5907578/dineshkumar',
                         'medium_url':'https://medium.com/@dineshkumarkb',
                         'linked_url':'https://www.linkedin.com/in/dineshkumar-k-b-b8853a24/',
@@ -25,6 +23,10 @@ def root_page(request):
                         "nav_link4":"Roles and Responsibilities",
                         "conviva_exp": CONVIVA,
                         "harman_exp" : HARMAN}
+
+
+def root_page(request):
+    resume_contents = CONTEXT_DICT
 
     return render(request,'cv_app/landing.html',context=resume_contents)
 
@@ -47,17 +49,6 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context.update({'resume_title': '''Python Cloud Developer. 11 years of industry experience in developing
-    applications using Python, Android | 
-    AWS Certified solutions architect associate |
-    Certified scrum master''',
-                        'stack_url':'https://stackoverflow.com/users/5907578/dineshkumar',
-                        'medium_url':'https://medium.com/@dineshkumarkb',
-                        'linked_url':'https://www.linkedin.com/in/dineshkumar-k-b-b8853a24/',
-                        'github_url':'https://github.com/dineshkumarkb',
-                        'nav_link1':"Career",
-                        'nav_link2':"Certifications",
-                        'nav_link3':"Personal Projects",
-                        "nav_link4":"Roles and Responsibilities"})
+        context.update(CONTEXT_DICT)
         return context
 
